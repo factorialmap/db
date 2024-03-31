6607bad5b7c374acd2589a260b08bb6aa3bf69a9
factorialmap <marcelo.carvalho.anjos@hotmail.com>
2024-03-31 00:39:19 -0300

    up ev sales yoy df

---
+++ create_data.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(scales)
+library(ggrepel)
+
+data_cars <- 
+  readxl::read_xlsx("C:/Users/Usuario/OneDrive/Documents/customers/carvalhoribeiro/db_cars.xlsx")
+
+
+write.csv(data_cars, file = "ev_sales_yoy.csv", row.names = FALSE)
